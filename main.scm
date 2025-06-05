43f2c9d7365d9977a07d64ff21396f27bf86be4a
piotrkwarcinski <piotr.kwarcinski@icloud.com>
2025-06-05 21:42:31 +0200

    Navigation tmux <> hx

--- main.scm
+++ main.scm
@@ -1,1 +1,34 @@
-(displayln "Hello world!")
+(require (prefix-in hx.static. "helix/static.scm"))
+(require (prefix-in hx.editor. "helix/editor.scm"))
+
+(define (exec-tmux-move dir)
+  (~> (command "tmux" `("select-pane" ,dir)) (spawn-process) (Ok->value) (wait)))
+
+(define (move-tmux dir)
+  (exec-tmux-move dir))
+
+(define (move hx-jump-fn tmux-dir-str)
+  (begin
+  (define view-p (hx.editor.editor-focus))
+  (hx-jump-fn)
+  (define view-n (hx.editor.editor-focus))
+  (if (equal? view-n view-p)
+      (move-tmux tmux-dir-str))))
+
+(define (move-right)
+  (move hx.static.jump_view_right "-R"))
+
+(define (move-left)
+  (move hx.static.jump_view_left "-L"))
+
+(define (move-down)
+  (move hx.static.jump_view_down "-D"))
+
+(define (move-up)
+  (move hx.static.jump_view_up "-U"))
+
+(provide
+  move-right
+  move-left
+  move-down
+  move-up)

a2b92b6758ceb22c7f1e1b3b4d5367e47bde575d
piotrkwarcinski <piotr.kwarcinski@icloud.com>
2025-07-24 00:47:24 +0200

    Prevent exiting zoomed-in helix pane

--- cog.scm
+++ cog.scm
@@ -1,4 +1,4 @@
 (define package-name 'hx-tmux-navigator)
 
-(define version "0.1.1")
+(define version "0.2.1")
 (define dependencies '())
--- navigator.scm
+++ navigator.scm
@@ -1,8 +1,29 @@
 (require (prefix-in hx.static. "helix/static.scm"))
 (require (prefix-in hx.editor. "helix/editor.scm"))
 
+;;@doc
+;; Prevents exiting zoom when the helix pane is zoomed-in.
+(define *prevent-zoom-exit* #t)
+
 (define (exec-tmux-move dir)
   (~> (command "tmux" `("select-pane" ,dir)) (spawn-process) (Ok->value) (wait)))
+
+(define (with-stdout-piped command)
+  (set-piped-stdout! command)
+  command)
+
+;;@doc
+;; Returns #t when the current pane is zoomed-in.
+(define (exec-tmux-zoomed?)
+  (define out (~> (command "tmux" '("display" "-p" "#{window_zoomed_flag}"))
+      (with-stdout-piped)
+      (spawn-process)
+      (Ok->value)
+      (wait->stdout)
+      (Ok->value)))
+  ;; Example output: "0\n"
+  ;; thus trim the newline.
+  (equal? (substring out 0 1) "1"))
 
 (define (move-tmux dir)
   (exec-tmux-move dir))
@@ -12,7 +33,7 @@
   (define view-p (hx.editor.editor-focus))
   (hx-jump-fn)
   (define view-n (hx.editor.editor-focus))
-  (if (equal? view-n view-p)
+  (if (and (equal? view-n view-p) (not (and *prevent-zoom-exit* (exec-tmux-zoomed?))))
       (move-tmux tmux-dir-str))))
 
 (define (move-right)
@@ -28,6 +49,7 @@
   (move hx.static.jump_view_up "-U"))
 
 (provide
+  *prevent-zoom-exit*
   move-right
   move-left
   move-down
